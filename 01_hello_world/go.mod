9babc4bfb941dff963c029dfdca5a70dae7ca20b
cec <cristian.escudero@primion-digitek.es>
2021-08-12 00:52:52 +0200

    Hello world

---
+++ 01_hello_world/go.mod
@@ -0,0 +1,5 @@
+// go mod init 01_hello_world/hello
+
+module 01_hello_world/hello
+
+go 1.16
---
+++ 01_hello_world/hello.go
@@ -0,0 +1,10 @@
+package main
+
+// Importing package fmt (format)
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Hello World")
+}
